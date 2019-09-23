12913358e67360ff38f9ed9141d71baaff864812
Natalya <natalyamyunster@yahoo.com>
2019-09-23 11:41:53 +0200

    added keydown and keyup

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ const score = document.querySelector('.score'),
     gameArea = document.querySelector('.gameArea');
 
 start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
 
 function startGame() {
     start.classList.add('hide');
+}
+
+function startRun() {
+    console.log('start');
+}
+
+function stopRun() {
+    console.log('stop');
 }
